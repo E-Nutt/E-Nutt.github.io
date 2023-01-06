a87e017f84760bc477a4f9420bf655ea8effb114
E-Nutt <this_is_E-Nutt@users.noreply.github.com>
2023-01-06 18:18:04 +0100

    name-title fixed

--- main_animation.js
+++ main_animation.js
@@ -1,36 +1,82 @@
-console.clear();
-var nameGrid = document.querySelector(".name-title")
+/* -----------------------------------function helper---------------------------- */
 
-/* -----------------------------------endless name---------------------------- */
+/*
+This helper function makes a group of elements animate along the x-axis in a seamless, responsive loop.
 
-var boxWidth = 40,
-    totalWidth = boxWidth * 10,
-    no1 = document.querySelectorAll("#name01 .nickname"),
-    no2 = document.querySelectorAll("#name01 .nickname"),
-    dirToRight = "+=" + totalWidth,
-    mod = gsap.utils.wrap(0, totalWidth)
+Features:
+ - Uses xPercent so that even if the widths change (like if the window gets resized), it should still work in most cases.
+ - When each item animates to the left or right enough, it will loop back to the other side
+ - Optionally pass in a config object with values like "speed" (default: 1, which travels at roughly 100 pixels per second), paused (boolean),  repeat, reversed, and paddingRight.
+ - The returned timeline will have the following methods added to it:
+   - next() - animates to the next element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
+   - previous() - animates to the previous element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
+   - toIndex() - pass in a zero-based index value of the element that it should animate to, and optionally pass in a vars object to control duration, easing, etc. Always goes in the shortest 
+   - current() - returns the current index (if an animation is in-progress, it reflects the final index)
+   - times - an Array of the times on the timeline where each element hits the "starting" spot. There's also a label added accordingly, so "label1" is when the 2nd element reaches the start.
+ */
 
-
-
-function marquee (which, time, direction){
-    gsap.set(which, {
-        x:function(i){
-            return i * boxWidth;
-        }
-    });
-    var action = gsap.timeline()
-        .to(which, {
-            x: direction,
-            modifier: {
-                x : x => mod(parseFloat(x)) + "px"
-            },
-            duration : time , ease: "none",
-            repeat : -1
-        })
-    return action;
+  
+   function horizontalLoop(items, config) {
+	items = gsap.utils.toArray(items);
+	config = config || {};
+	let tl = gsap.timeline({repeat: config.repeat, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)}),
+		length = items.length,
+		startX = items[0].offsetLeft,
+		times = [],
+		widths = [],
+		xPercents = [],
+		curIndex = 0,
+		pixelsPerSecond = (config.speed || 1) * 100,
+		snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+		totalWidth, curX, distanceToStart, distanceToLoop, item, i;
+	gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+		xPercent: (i, el) => {
+			let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+			xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
+			return xPercents[i];
+		}
+	});
+	gsap.set(items, {x: 0});
+	totalWidth = items[length-1].offsetLeft + xPercents[length-1] / 100 * widths[length-1] - startX + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+	for (i = 0; i < length; i++) {
+		item = items[i];
+		curX = xPercents[i] / 100 * widths[i];
+		distanceToStart = item.offsetLeft + curX - startX;
+		distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+		tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
+		  .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
+		  .add("label" + i, distanceToStart / pixelsPerSecond);
+		times[i] = distanceToStart / pixelsPerSecond;
+	}
+	function toIndex(index, vars) {
+		vars = vars || {};
+		(Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
+		let newIndex = gsap.utils.wrap(0, length, index),
+			time = times[newIndex];
+		if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
+			vars.modifiers = {time: gsap.utils.wrap(0, tl.duration())};
+			time += tl.duration() * (index > curIndex ? 1 : -1);
+		}
+		curIndex = newIndex;
+		vars.overwrite = true;
+		return tl.tweenTo(time, vars);
+	}
+	tl.next = vars => toIndex(curIndex+1, vars);
+	tl.previous = vars => toIndex(curIndex-1, vars);
+	tl.current = () => curIndex;
+	tl.toIndex = (index, vars) => toIndex(index, vars);
+	tl.times = times;
+	tl.progress(1, true).progress(0, true); // pre-render for performance
+	if (config.reversed) {
+	  tl.vars.onReverseComplete();
+	  tl.reverse();
+	}
+	return tl;
 }
-gsap.set(".wrapper", {x: -500})
-gsap.set([".greet", "#enut"], {autoAlpha:1})
+
+/* ----------------------------- end function helper ---------------------------*/
+
+gsap.set([".greet"], {autoAlpha:1})
 
 let mm = gsap.matchMedia();
     
@@ -40,16 +86,11 @@ mm.add("(min-width:600px)", () => {
     .from(".greet", {y : -180, stagger:1, ease :"back"})
     .to(".greet", {y : 180, stagger:1},1)
 
-    
-    var nameTitle = gsap.timeline({paused: true})
-        .add(marquee([no1], 10, dirToRight))
-        
-    
-    /* .fromTo("#enut", {scale:0},{scale:1},0)
-    .to("#raf", {scale:0,},0) */
-
-    nameGrid.addEventListener("mouseenter", () => nameTitle.play())
-    nameGrid.addEventListener("mouseleave", () => nameTitle.pause())
+    var nameGrid = document.querySelector(".name-title")
+    var nicknames = gsap.utils.toArray(".nickname");
+    const loop = horizontalLoop(nicknames, {paused: true, repeat:-1})
+    nameGrid.addEventListener("mouseenter", () => loop.play())
+    nameGrid.addEventListener("mouseleave", () => loop.pause())
 })
 
 
@@ -58,6 +99,10 @@ mm.add("(max-width:600px)", () => {
     var tl = gsap.timeline({repeat: 15, yoyo:true, repeatDelay:0.7, autoAlpha:1})
     .from(".greet", {y : -100, stagger:1, ease :"back"})
     .to(".greet", {y : 100, stagger:1},1)
+
+    var nicknames = gsap.utils.toArray(".nickname");
+    const loop1 = horizontalLoop(nicknames, {repeat: -1})
+    loop1.play();
 }) 
 
 
